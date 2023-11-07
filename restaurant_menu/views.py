1fc38ea1c27416e94dac99d5a62cdb1ef2390500
yashkumargupta <guptayashyg45227@gmail.com>
2023-11-07 21:11:29 +0530

    Created a admin page.

--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -6,6 +6,11 @@ class MenuList(generic.ListView):
     queryset = Item.objects.order_by("-date_created")
     template_name = "index.html"
 
+    def get_context_data(self):
+        context = {"meals": ["Pizza", "Pasta"],
+                   "ingredients": ["things"]}
+        return context
+
 
 class MenuItemDetail(generic.DetailView):
     model = Item
